7495a8cc818680e311dc1eb163f7bdce598be85b
DanilKustov <132808795+DanilKustov@users.noreply.github.com>
2024-01-20 18:27:37 +0300

    Update hw-6.py

--- hw-6.py
+++ hw-6.py
@@ -1,10 +1,21 @@
+from decimal import Decimal
+
+
 class Account:
-    def __init__(self, name):
+    def __init__(self, name, balance):
         self.name = name
-        self.balance = 1.0
+        self.__balance = round(Decimal(balance),2)
+        print(f"Банковский счет на имя {self.name} создан. Текущий баланс: {self.__balance}")
 
     def withdraw_money(self, money):
-        self.balance = self.balance - money
+        if self.__balance >= money:
+            self.__balance = round(Decimal(self.__balance) - Decimal(money), 2)
+            print(f"Произведено снятие. Текущий баланс: {self.__balance}")
+        else:
+            print(f"Недостаточно средств на счете. Текущий баланс: {self.__balance}")
 
     def put_money(self, money):
-        self.balance = self.balance + money
+        self.__balance = round(Decimal(self.__balance) + Decimal(money), 2)
+        print(f"Счет пополнен. Текущий баланс: {self.__balance}")
+
+
